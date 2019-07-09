2def8992284c08af56559953509ba620f89b964a
atul.vishnu.kurkure <atul.vishnu.kurkure@accenture.com>
2019-07-09 12:18:29 +0530

    Clearing messages

--- src/mainFile.java
+++ src/mainFile.java
@@ -4,8 +4,7 @@ public class mainFile {
 	public static void main(String[] args) {
 	// this is the fist comment
 		System.out.println("Prining the log here");
-		System.out.println("Prining the log here");
-		System.out.println("Thinsis my mainfile");
+	
 		
 	}
 
